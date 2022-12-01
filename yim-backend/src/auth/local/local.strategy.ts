a668ae4fe670e836709c20861e61d63e2eceb667
pechkr2020 <pechjie3238@gmail.com>
2022-12-01 11:44:51 +0700

    Rebuild login-res

--- yim-backend/src/auth/local/local.strategy.ts
+++ yim-backend/src/auth/local/local.strategy.ts
@@ -11,7 +11,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
-    console.log(password)
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException({
