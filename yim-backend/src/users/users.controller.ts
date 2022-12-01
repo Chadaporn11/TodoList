2a9c3b922b4f23c7cee266bc5e8595132253332c
pechkr2020 <pechjie3238@gmail.com>
2022-12-01 23:39:57 +0700

    Delete Role

--- yim-backend/src/users/users.controller.ts
+++ yim-backend/src/users/users.controller.ts
@@ -13,8 +13,6 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
-import { Role } from 'src/role/role.enum';
-import { Roles } from 'src/role/role.decorator';
 
 @UseGuards(JwtAuthGuard)
 @Controller('users')
@@ -22,7 +20,6 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('createUser')
-  @Roles(Role.Admin)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -44,13 +41,11 @@ export class UsersController {
   }
 
   @Patch('updateUser/:id')
-  @Roles(Role.Admin)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete('deleteUser/:id')
-  @Roles(Role.Admin)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
