eb59dee506b11e51c4e11a9382b0ca4312a624df
pechkr2020 <pechjie3238@gmail.com>
2022-12-01 18:48:01 +0700

    Adding JWT-Guard

--- yim-backend/src/group/group.module.ts
+++ yim-backend/src/group/group.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
