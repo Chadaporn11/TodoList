456e6d1569b79acb33e60803a0ee1a30400069bb
bankvery007 <bunyarithvery007@hotmail.com>
2022-12-01 11:49:19 +0700

    signin

--- frontend/src/components/cards/CardUserList.tsx
+++ frontend/src/components/cards/CardUserList.tsx
@@ -1,55 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CardUserList.css';
-//ant design
-import { Card, Avatar } from 'antd';
+import { Card, Row, Col, List } from 'antd';
 import { UserOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
-
-
+import { Avatar } from 'antd';
+import { userInterface } from '../../models/IUser';
 const CardUserList = () => {
+
+    const [Users, setUsers] = useState<userInterface[]>([]);
+
+    const apiUrl = "http://localhost:8080";
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            // Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+        },
+    };
+
+    const getUser = async () => {
+        fetch("http://localhost:8080/users/listUsers", requestOptions)
+            .then((response) => response.json())
+            .then((res) => {
+                if (res) {
+                    setUsers(res);
+                    console.log(res);
+                } else {
+                    console.log('else')
+                }
+            });
+    };
+
+    useEffect(() => {
+        getUser();
+
+    }, []);
 
     return (
         <>
-            <Card bodyStyle={{ width: '1100px', height: '80px', display: 'flex', alignItems: 'center' }}>
-                <div className='card-img'>
-                    <Avatar
-                        shape="square"
-                        size={55}
-                        icon={<UserOutlined />} />
+            <List
+                grid={{
+               
+                    lg: 24,
+               
+                }}
+                bordered={false}
+                dataSource={Users}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Card bodyStyle={{ width: '1100px', height: '80px', display: 'flex', alignItems: 'center' }}>
+                            <div className='card-img'>
+                                <Avatar
+                                    shape="square"
+                                    size={55}
+                                    icon={<UserOutlined />} />
 
-                </div>
-                <div className='card-name'>
-                    <h3>Name</h3>
-                </div>
-                <div className='card-title'>
-                    <p>card title</p>
-                </div>
-                <div className='card-action'>
-                    <EditOutlined />
-                    <DeleteOutlined />
-                </div>
+                            </div>
+                            <div className='card-name'>
+                                <h3>{item.username}</h3>
+                            </div>
+                            <div className='card-title'>
+                                <p>{item.address}</p>
+                            </div>
+                            <div className='card-action'>
+                                <EditOutlined />
+                                <DeleteOutlined />
+                            </div>
 
-            </Card>
-            <br/>
-            <Card bodyStyle={{ width: '1100px', height: '80px', display: 'flex', alignItems: 'center' }}>
-                <div className='card-img'>
-                    <Avatar
-                        shape="square"
-                        size={55}
-                        icon={<UserOutlined />} />
+                        </Card>
+                    </List.Item>
+                )}
+            />
 
-                </div>
-                <div className='card-name'>
-                    <h3>Name</h3>
-                </div>
-                <div className='card-title'>
-                    <p>card title</p>
-                </div>
-                <div className='card-action'>
-                    <EditOutlined />
-                    <DeleteOutlined />
-                </div>
 
-            </Card>
+
+
         </>
     )
 }
