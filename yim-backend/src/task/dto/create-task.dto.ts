2b70a2d8d505d7c44f08beb4ec5fd2c7f6cd4e89
pechkr2020 <pechjie3238@gmail.com>
2022-12-01 16:09:26 +0700

    modified tasks

--- yim-backend/src/task/dto/create-task.dto.ts
+++ yim-backend/src/task/dto/create-task.dto.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { IsNotEmpty } from 'class-validator';
 import { Group } from 'src/group/entities/group.entity';
 import { User } from 'src/users/entities/user.entity';
@@ -6,8 +7,6 @@ export class CreateTaskDto {
   id: number;
   @IsNotEmpty()
   name: string;
-  @IsNotEmpty()
-  todo: string
   user: User;
   group: Group;
 }
