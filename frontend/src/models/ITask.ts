76d67740c6573bb6db3374ba25e8cf68f9e81a72
bankvery007 <bunyarithvery007@hotmail.com>
2022-12-04 22:02:43 +0700

    update img layout

--- frontend/src/models/ITask.ts
+++ frontend/src/models/ITask.ts
@@ -2,6 +2,7 @@ import { userInterface } from "./IUser";
 import { GroupInterface } from "./IGroup";
 
 export interface TaskInterface {
+    length: any;
     id: number,
     name: string,
     userId: userInterface,
